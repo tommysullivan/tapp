c17657c8403221acae2d02b9d0a3b38eeb7c46d5
Sam Xiao <sam.xiao@lookout.com>
2015-02-17 14:13:25 -0800

    We don’t need favicon for now.
    Signed-off-by: Sam Xiao <sam.xiao@lookout.com>

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -9,8 +8,6 @@ var routes = require('./routes/index');
 
 var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
