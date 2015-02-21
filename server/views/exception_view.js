619b8e29649f765a8932d7efd798b33ead71129d
tsullivan <thomas.sullivan@lookout.com>
2015-02-20 16:58:36 -0800

    deleted comment
    JIRA: AUTO-225

--- server/views/exception_view.js
+++ server/views/exception_view.js
@@ -3,7 +3,6 @@ module.exports = function ExceptionView(defaultErrorContentType) {
         render: function(exception, response) {
             var contentTypeWithoutFormat = exception.isPAError ? exception.contentType : defaultErrorContentType;
             response.append('Content-Type', contentTypeWithoutFormat + '+text; charset=utf-8');
-            //response.append('Content-Type', 'text');
             response.statusCode = 403;
             response.end(exception.message);
         }
